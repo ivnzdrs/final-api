54f69df5ce84e53fb9da3171b4eec8b6b1f9fe03
ivnzdrs <ivandres.student@ua.edu.ph>
2025-05-17 22:19:17 +0800

    new

--- src/main/java/com/andres/post/PostController.java
+++ src/main/java/com/andres/post/PostController.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173", "https://final-ui-i8ck.onrender.com"})
+@CrossOrigin(origins = {"http://localhost:5173", "https://final-ui-ce9r.onrender.com"})
 @RequestMapping("/andres/posts")
 public class PostController {
 
